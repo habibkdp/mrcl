36008f5a718611e62fcfc157ea504eaab2c7ecb9
habibkdp <habibkdp9@gmail.com>
2019-05-15 23:40:12 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+$email = "ekayanamarchell@gmail.com";
+$password = "Qriket1";
+
+//Pillih Warna 0 untuk hijau 1 untuk kuning
+$collor = "0";
+
+// Pilih Jumlah Spin Saran 1 - 5
+$amount = "3";
+
+
+
